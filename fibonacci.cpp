a1f78f60499e785876d537a49e2bc25ee79b1148
VAIBHAV S HEGDE <35395147+vaibhavhegde15@users.noreply.github.com>
2019-02-09 10:52:26 +0530

    Update and rename fibonacci.c to fibonacci.cpp

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -1,11 +1,10 @@
 int main{
   int n;
-  scanf("%d",&n);
+  cin >> n;
   int a[n+1];
   a[0]=0;
   a[1]=1;
-  int i;
-  for(i=2;i<=n;i++){
+  for(int i=2;i<=n;i++){
       a[i]=a[i-1]+a[i-2];
   }
 }
